a6ccb2bea594f4ed179251ab9ac6d5d61dec7fde
Jamie Alexandre <jamalex@gmail.com>
2014-03-20 11:27:29 -0700

    Use service instead of script for starting/stopping hotspot.

--- create_udev_rule.sh
+++ create_udev_rule.sh
@@ -1,5 +1,5 @@
 cat <<EOF | tee "/etc/udev/rules.d/95-ka-lite-hotspot.rules" > /dev/null 2>&1
-ACTION=="add", SUBSYSTEM=="usb", ENV{ID_VENDOR_ID}=="148f", RUN+="`pwd`/ka-lite-hotspot restart"
-ACTION=="remove", SUBSYSTEM=="usb", ENV{ID_VENDOR_ID}=="148f", RUN+="`pwd`/ka-lite-hotspot stop"
+ACTION=="add", SUBSYSTEM=="usb", ENV{ID_VENDOR_ID}=="148f", RUN+="service ka-lite-hotspot restart"
+ACTION=="remove", SUBSYSTEM=="usb", ENV{ID_VENDOR_ID}=="148f", RUN+="service ka-lite-hotspot stop"
 EOF
 
